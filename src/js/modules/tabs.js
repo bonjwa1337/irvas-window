133aac09d82983c7bc9551bd134158d5a66d5cc2
Ivan Vasenkin <bonjwa1447@yandex.ru>
2021-12-28 15:02:05 +0300

    adding tabs toggle

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,39 @@
+﻿const tabs = (headerSelector, tabSelector, contentSelector, active) => {
+    const header = document.querySelector(headerSelector),
+        tab = document.querySelectorAll(tabSelector),
+        content = document.querySelectorAll(contentSelector);
+
+    function hideTabContent() {
+        content.forEach(item => {
+            item.style.display = 'none';
+        })
+
+        tab.forEach(item => {
+            item.classList.remove(active);
+        })
+    }
+
+    function showTabContent(i = 0) {
+        tab[i].classList.add(active);
+        content[i].style.display = 'block';
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    header.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target &&
+            (target.classList.contains(tabSelector.replace(/\./, "")) || 
+        target.parentNode.classList.contains(tabSelector.replace(/\./, "")))) {
+            tab.forEach((item, i) => {
+                if (target == item || target.parentNode == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+};
+
+export default tabs;
